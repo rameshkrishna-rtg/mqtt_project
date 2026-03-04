0665805f7ec912665c25f3ca331c17df04a4e501
rameshkrishna-rtg <rameshkrishnartg@gmail.com>
2026-03-04 15:55:26 +0530

    Initial commit

---
+++ publish.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpMqtt\Client\MqttClient;
+
+$server   = 'localhost'; 
+$port     = 1883;
+$clientId = 'php_test_' . uniqid();
+
+$mqtt = new MqttClient($server, $port, $clientId);
+$mqtt->connect();
+
+$mqtt->subscribe('connect/check', function ($topic, $message) {
+    echo "Received: {$message}\n";
+}, 0);
+
+$mqtt->publish('connect/check', 'From publisher', 0);
+
+$mqtt->loop(true);
